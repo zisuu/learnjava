a296f342edbeceb25d92ae03683a53d5d33ab664
dazi_support <d.ziswiler@unico.ch>
2022-04-19 06:56:20 +0200

    ternaer

---
+++ labproject/labproject/src/ternaer.java
@@ -0,0 +1,7 @@
+
+public class ternaer {
+    public static void main(String[] args) {
+        int zahl = 10;
+        System.out.println("Die Zahl ist " + (zahl % 2 == 0 ? "gerade" : "ungerade"));
+    }
+}
