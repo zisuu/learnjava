ba47b02f8d361fb6047c9ef8eb62727c4228dbe2
dazi_support <d.ziswiler@unico.ch>
2022-04-21 08:22:03 +0200

    wip kgv

--- labproject/labproject/src/Kap4/ggt.java
+++ labproject/labproject/src/Kap4/ggt.java
@@ -6,10 +6,10 @@ public class ggt {
     }
 
     private static int ggt(int zahl1, int zahl2) {
-        if (zahl1 <= 0 || zahl2 <= 0) 
-         throw new IllegalArgumentException("Beide Zahlen müssen >0 sein.");
+        if (zahl1 <= 0 || zahl2 <= 0)
+            throw new IllegalArgumentException("Beide Zahlen müssen >0 sein.");
         int kandidat = zahl1 < zahl2 ? zahl1 : zahl2;
-        while(zahl1 % kandidat != 0 || zahl2 % kandidat != 0){
+        while (zahl1 % kandidat != 0 || zahl2 % kandidat != 0) {
             kandidat--;
         }
         return kandidat;
