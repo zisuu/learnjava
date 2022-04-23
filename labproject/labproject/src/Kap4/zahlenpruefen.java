edf7131fb5fe1dbfbdc7a9eee66bd290600d9458
dazi_support <d.ziswiler@unico.ch>
2022-04-23 08:49:55 +0200

    update zahlenpruefen

--- labproject/labproject/src/Kap4/zahlenpruefen.java
+++ labproject/labproject/src/Kap4/zahlenpruefen.java
@@ -2,16 +2,6 @@ package Kap4;
 
 import java.io.*;
 
-// Wenn Sie in den bisherigen Beispielen eine Zahl eingeben sollten, aber dann etwas anderes eingegeben haben, 
-// kam es zu einer sehr unschönen NumberFormatException. In Kapitel 9, »Fehler und Ausnahmen«, werden Sie zwar 
-// lernen, mit dieser Fehlermeldung umzugehen, aber schon jetzt können Sie in einem String Zeichen für Zeichen 
-// prüfen, ob es sich um eine Zahl handelt. Sie können Strings ähnlich wie Arrays behandeln: Die Methode length
-//  – Vorsicht, bei Strings ist es eine Methode – gibt an, wie viele Zeichen im String enthalten sind, die Methode 
-//  charAt gibt das Zeichen an der angegebenen Stelle zurück.
-// Schreiben Sie ein Programm, das den Benutzer einen String eingeben lässt, das prüft, ob dieser String eine 
-// gültige Zahl ist, und das eine entsprechende Meldung ausgibt. Die Lösung zu dieser Übung finden Sie im Anhang.
-
-
  /**
  * Programm das den Benutzer einen String eingeben lässt, das prüft, ob dieser String eine gültige Zahl ist, 
  * und das eine entsprechende Meldung ausgibt. 
@@ -30,7 +20,7 @@ public class zahlenpruefen {
 
     public static void pruefeEingabe(String eingabe) {
         for (int i = 0; i < eingabe.length(); i++) {
-            System.out.println(Character.isDigit(eingabe.charAt(i)) ? eingabe.charAt(i) + " is Digit" : eingabe.charAt(i) + " is not Digit");
+            System.out.println(Character.isDigit(eingabe.charAt(i)) ? eingabe.charAt(i) + " ist eine Zahl" : eingabe.charAt(i) + " ist ein Buchstaben");
         }
     }
 }
