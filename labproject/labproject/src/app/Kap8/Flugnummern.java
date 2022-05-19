51a77aa8bda6e08715437641fb1c055ae1612f0e
zisuu <david.ziswiler@finecloud.ch>
2022-05-19 17:31:31 +0200

    wip Flugnummern

---
+++ labproject/labproject/src/app/Kap8/Flugnummern.java
@@ -0,0 +1,13 @@
+package app.Kap8;
+
+/**
+ * Schreiben Sie eine Methode, die reguläre Ausdrücke benutzt, um ein String-Array – Ihr Postfach – 
+ * nach der ersten Flugnummer zu durchsuchen. Eine Flugnummer besteht immer aus zwei Großbuchstaben, 
+ * gefolgt von einem Bindestrich, gefolgt von einer Reihe von Ziffern.
+ * 
+ * @author Dave
+ * @version 1.0
+ */
+public class Flugnummern {
+    
+}
