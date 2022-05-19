19957513c07ac79dd24b8f534226d196d3cc3493
zisuu <david.ziswiler@finecloud.ch>
2022-05-19 21:31:24 +0200

    add flight number checker

--- labproject/labproject/src/app/Kap8/Flugnummern.java
+++ labproject/labproject/src/app/Kap8/Flugnummern.java
@@ -1,5 +1,7 @@
 package app.Kap8;
 
+import java.util.regex.*;
+
 /**
  * Schreiben Sie eine Methode, die reguläre Ausdrücke benutzt, um ein String-Array – Ihr Postfach – 
  * nach der ersten Flugnummer zu durchsuchen. Eine Flugnummer besteht immer aus zwei Großbuchstaben, 
@@ -9,5 +11,32 @@ package app.Kap8;
  * @version 1.0
  */
 public class Flugnummern {
+    public static void main(String[] args) {
+        String[] postfach = new String []{"Hans Fischer", "RTestmail 12235", "HE-877665436", "sdgsdfas124w-as12", "ZH-454", "-BE-1111"};
+        findFlightNumber(postfach);
+    }
+
+    /**
+     * Durchsucht das StringArray postFach nach Flugnummern.
+     *
+     * @param postfach als StringArray
+     * @return die Flugnummern
+     * @throws IllegalArgumentException wenn keine Flugnummern gefunden
+     */
+    public static void findFlightNumber(String [] postfach){
+        String flightNumber = "";
+         // TODO: Regex Anpassen    
+        Pattern FLIGHTNUMBER_PATTERN = Pattern.compile("(^[A-Z]{2}[-]\\d+)");
+        if (postfach == null){
+            throw new IllegalArgumentException("Postfach darf nicht null sein");
+        }
+        for (int i = 0; i < postfach.length; i++) {
+            Matcher matcher = FLIGHTNUMBER_PATTERN.matcher(postfach[i]);
+            if (matcher.find()) {
+                String result = matcher.group(0);
+                System.out.println(result);
+            }
+        }
+    }
     
 }
---
+++ labproject/labproject/src/test/FlugnummernTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import app.Kap8.Flugnummern;
+
+public class FlugnummernTest {
+
+    // @Test
+    // public void normal() {
+    //     String[] postfach = new String []{"Hans Fischer", "RTestmail 12235", "HE-877665436", "sdgsdfas124w-as12", "ZH-454", "-BE-1111"};
+    //     String result = "HE-877665436,ZH-454";
+    //     assertEquals(result, Flugnummern.findFlightNumber(postfach));
+    // }
+}
